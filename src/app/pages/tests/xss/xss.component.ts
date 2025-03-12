4e441a5dde93fb992859e6abb7122836339fc7f4
lorinspan <lorinspanx@gmail.com>
2025-03-12 22:00:36 +0200

    Fixed validation on inputs and buttons.

--- src/app/pages/tests/xss/xss.component.ts
+++ src/app/pages/tests/xss/xss.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { FeedbackService } from '../../../services/feedback.service';
 import { AuthService } from '../../../services/auth.service';
@@ -9,45 +9,55 @@ import { DomSanitizer } from '@angular/platform-browser';
   selector: 'app-xss',
   standalone: true,
   templateUrl: './xss.component.html',
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, ReactiveFormsModule],
   styleUrls: ['./xss.component.css']
 })
 export class XssComponent implements OnInit {
-  name: string = '';
-  message: string = '';
+  feedbackForm!: FormGroup;
   feedbackList: any[] = [];
   isLoggedIn: boolean = false;
 
   constructor(
     private feedbackService: FeedbackService,
     private authService: AuthService,
-    private sanitizer: DomSanitizer
+    private sanitizer: DomSanitizer,
+    private formBuilder: FormBuilder
   ) {}
 
   ngOnInit() {
+    this.initForm();
     this.loadFeedback();
-    this.checkUserStatus();
+    this.checkUser();
   }
 
-  checkUserStatus() {
+  initForm() {
+    this.feedbackForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      message: ['', Validators.required]
+    });
+  }
+
+  checkUser() {
     this.isLoggedIn = this.authService.isAuthenticated();
     if (this.isLoggedIn) {
       this.authService.getLoggedInUser().subscribe(user => {
         if (user) {
-          this.name = user.username;
+          this.feedbackForm.patchValue({ name: user.username });
+          this.feedbackForm.get('name')?.disable({emitEvent: false});
         }
       });
     }
   }
 
   submitFeedback() {
-    this.feedbackService.submitFeedback(this.name, this.message).subscribe(response => {
-      if (response?.error) {
-        console.log(response.error);
-      } else {
-        this.message = '';
-        this.loadFeedback();
-      }
+    if (this.feedbackForm.invalid) {
+      return;
+    }
+
+    const { name, message } = this.feedbackForm.value;
+    this.feedbackService.submitFeedback(name, message).subscribe(() => {
+      this.feedbackForm.patchValue({ message: '' });
+      this.loadFeedback();
     });
   }
 
